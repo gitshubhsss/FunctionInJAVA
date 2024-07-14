93188d8db03312a3795ff84e83e75b0f16a51f38
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-14 21:04:23 +0530

    type casting

---
+++ javaBasics.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * javaBasics
+ */
+public class javaBasics {
+    public static void main(String[] args) {
+        char a='a';
+        short b=50;
+        int sum=a+b;
+        System.out.print("sum of "+(int)a+" + "+b+" = "+sum);
+
+        //147
+    }
+}
