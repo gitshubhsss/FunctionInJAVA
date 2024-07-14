fc11a9ce6cdf8e5fc7e8c445b0894c7e90cb858d
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-14 21:03:43 +0530

    greeting funtion

---
+++ greeting.java
@@ -0,0 +1,11 @@
+
+public class greeting {
+    public static void main(String[] args) {
+        greetingCard("shubham ","ranjane");
+        greetingCard("omkar ","daswadkar");
+    }
+    public static void greetingCard(String firstName,String lastName){
+        System.out.println("good evening "+ firstName + " "+ lastName);
+    }
+   
+}
